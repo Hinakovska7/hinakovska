6b4dbac672c2ce9b4c4ab86002767f46f9a44a77
Hinakovska7 <akaishisummer7@gmail.com>
2025-09-14 19:32:07 +0900

    test

--- js/include.js
+++ js/include.js
@@ -5,7 +5,7 @@
  */
 
 document.addEventListener('DOMContentLoaded', function() {
-    fetch('/hinakovska/hinakovska/include/header.html')
+    fetch('include/header.html')
         .then(res =>  {
             if (res.ok) {
                 return res.text()
@@ -17,7 +17,7 @@ document.addEventListener('DOMContentLoaded', function() {
 }, false);
     
 document.addEventListener('DOMContentLoaded', function() {
-    fetch('/hinakovska/include/footer.html')
+    fetch('include/footer.html')
         .then(res =>  {
             if (res.ok) {
                 return res.text()
