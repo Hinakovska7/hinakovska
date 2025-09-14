476b9795e3b89e52aaec1c376d9808144120171a
Hinakovska7 <akaishisummer7@gmail.com>
2025-09-14 15:35:53 +0900

    test

---
+++ WebContents/js/include.js
@@ -0,0 +1,29 @@
+/**
+ * header.htmlを読み込む
+ * footer.htmlを読み込む
+ * すべてのHTMLファイルの<header>と<footer>にそれぞれheader.htmlとfooter.htmlを埋め込む
+ */
+
+document.addEventListener('DOMContentLoaded', function() {
+    fetch('../include/header.html')
+        .then(res =>  {
+            if (res.ok) {
+                return res.text()
+            }
+            throw new Error('指定のリソースが無効です。');
+        })
+        .then(html => document.getElementById('header').innerHTML = html)
+        .catch(e => window.alert(`例外発生：${e.message}`))
+}, false);
+    
+document.addEventListener('DOMContentLoaded', function() {
+    fetch('../include/footer.html')
+        .then(res =>  {
+            if (res.ok) {
+                return res.text()
+            }
+            throw new Error('指定のリソースが無効です。');
+        })
+        .then(html => document.getElementById('footer').innerHTML = html)
+        .catch(e => window.alert(`例外発生：${e.message}`))
+}, false);
